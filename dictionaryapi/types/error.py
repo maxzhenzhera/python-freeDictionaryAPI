c95846a72de948e7fd0c60b7117d4254ca2d5a1d
Max Zhenzhera <megashurik@urk.net>
2021-05-08 14:40:37 +0300

    add parsed object for API errors

---
+++ dictionaryapi/types/error.py
@@ -0,0 +1,43 @@
+"""
+Contains error type (of json API response, meaning parsed object).
+
+.. class Error(ParsedObject)
+    Implements error type (info about API error)
+"""
+
+from .base import ParsedObject
+
+
+__all__ = ['Error']
+
+
+class Error(ParsedObject):
+    """
+    Implements the object of API json response
+    that consists of error data:
+        * title - short error title;
+        * message - more detailed error message;
+        * resolution - offer to solve problem.
+
+    .. property:: title(self) -> str
+    .. property:: message(self) -> str
+    .. property:: resolution(self) -> str
+    """
+
+    def __repr__(self) -> str:
+        return f'Error(title={self.title!r}, message={self.message!r}, resolution={self.resolution!r})'
+
+    @property
+    def title(self) -> str:
+        """ Get title """
+        return self._data.get('title')
+
+    @property
+    def message(self) -> str:
+        """ Get message """
+        return self._data.get('message')
+
+    @property
+    def resolution(self) -> str:
+        """ Get resolution """
+        return self._data.get('resolution')
