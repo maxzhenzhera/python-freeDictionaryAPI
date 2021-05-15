f08e611b29fb55e3d8e2e6691d9d0644c3897fe5
Max Zhenzhera <megashurik@urk.net>
2021-05-15 13:00:21 +0300

    correct brazilian-portuguese code value

--- dictionaryapi/languages.py
+++ dictionaryapi/languages.py
@@ -15,7 +15,7 @@ __all__ = ['LanguageCodes', 'DEFAULT_LANGUAGE_CODE']
 class LanguageCodes(Enum):
     """ Contains supported languages and codes that used in API url """
     ARABIC = 'ar'
-    BRAZILIAN_PORTUGUESE = 'pt - BR'
+    BRAZILIAN_PORTUGUESE = 'pt-BR'
     ENGLISH_UK = 'en_GB'
     ENGLISH_US = 'en_US'
     FRENCH = 'fr'
